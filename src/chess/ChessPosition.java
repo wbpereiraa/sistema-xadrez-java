a2c5984f6adf82be0f403c1fbd6ae987542ce915
William Barbosa <wb.pereiraa@gmail.com>
2024-02-10 19:58:06 -0300

    Pequena melhoria na impressão do tabuleiro

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,8 +8,8 @@ public class ChessPosition {
     private  int row;
 
     public ChessPosition(char column, int row){
-        if(column < 'a' || column > 'h' || row < 1 || row >8){
-            throw new ChessException("Erro ao instanciar a posição do xadrez. O intervalo válido é de a1 até h8.")
+        if(column < 'a' || column > 'h' || row < 1 || row > 8){
+            throw new ChessException("Erro ao instanciar a posição do xadrez. O intervalo válido é de a1 até h8.");
         }
         this.column = column;
         this.row = row;
@@ -23,10 +23,10 @@ public class ChessPosition {
     }
 
     protected Position toPosition(){
-        return  new Position(8 - row, column - 'a');
+        return new Position(8 - row, column - 'a');
     }
     protected  static  ChessPosition fromPosition(Position position){
-        return  new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+        return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
     }
 
     @Override
